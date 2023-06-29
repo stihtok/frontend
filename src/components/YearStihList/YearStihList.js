e6f07f40ba530850631edb7d5672b9b21458164e
Roman Orlov <admin@4te.me>
2023-06-29 18:56:48 +0300

    YearPage error handle

--- src/components/YearStihList/YearStihList.js
+++ src/components/YearStihList/YearStihList.js
@@ -2,10 +2,13 @@ import "./YearStihList.css";
 import StihListByAuthor from "./StihListByAuthor";
 import { useState, useEffect } from "react";
 import ky from "ky";
+import Loading from "../Loading";
+import ErrorPage from "../error-page";
 
 function YearStihList(props) {
   let [authors, setAuthors] = useState([]);
   let [isLoading, setIsLoading] = useState(true);
+  let [isError, setIsError] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
@@ -16,19 +19,30 @@ function YearStihList(props) {
     })
     .finally(() => setIsLoading(false))
     .catch((error) => {
-      console.log(error)
+      console.log(error);
+      setIsError(true);
     });
   }, []);
 
+  if (isError) return <ErrorPage />;
 
-  return (
-    <div className="yearStihList">
-      {authors.map((author) => {
+  function PageContent() {
+    if (isLoading) {
+      return <Loading />
+    } else {
+     return(
+        authors.map((author) => {
         return (
           <StihListByAuthor authorId={author.id} authorName={author.name} authorImg={author.photo} stihs={author.stihs}/>
         )
       })
+      )
     }
+  }
+
+  return (
+    <div className="yearStihList">
+        <PageContent />
     </div>
   );
 }
