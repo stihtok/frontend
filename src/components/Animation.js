d62d73169f51951057966e4a2be2e4e32aeaa1ca
Roman Orlov <admin@4te.me>
2024-02-01 03:24:04 +0300

    0.5 animation

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -8,7 +8,7 @@ const Animation = ({ children }) => {
         initial={{ opacity:0 }}
         animate={{ opacity:1 }}
         exit={{ opacity:0 }}
-        transition={{ transition: "ease-in", duration: 0.6 }}
+        transition={{ transition: "ease-in", duration: 0.5 }}
     >
       {children}
     </motion.div>
